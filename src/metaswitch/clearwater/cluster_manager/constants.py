4bc24287689692336f20bcb0fb51475828510291
Graeme Robertson <graeme.robertson@metaswitch.com>
2015-04-30 11:35:49 +0000

    Add invalid cluster state constant

--- src/metaswitch/clearwater/cluster_manager/constants.py
+++ src/metaswitch/clearwater/cluster_manager/constants.py
@@ -8,6 +8,7 @@ LEAVE_PENDING = "leave pending"
 STARTED_LEAVING = "started leaving"
 LEAVING_CONFIG_CHANGING = "leaving, config changing"
 LEAVING_RESYNCING = "leaving, resyncing"
+INVALID_CLUSTER_STATE = "invalid cluster state"
 
 # Node states
 WAITING_TO_JOIN = "waiting to join"
