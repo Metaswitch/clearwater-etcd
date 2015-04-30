a59a2f36b8124a9a7dc44a552b2748ff59cf878d
Rob Day <Robert.Day@metaswitch.com>
2015-04-30 09:05:55 +0000

    SIGQUIT handling

--- src/metaswitch/clearwater/cluster_manager/main.py
+++ src/metaswitch/clearwater/cluster_manager/main.py
@@ -31,6 +31,18 @@ LOG_LEVELS = {'0': logging.CRITICAL,
               '3': logging.INFO,
               '4': logging.DEBUG}
 
+def install_sigquit_handler(plugins):
+    """
+    Install SIGUSR1 handler to dump stack."
+    """
+    def sigquit_handler(sig, stack):
+        """
+        Handle SIGUSR1 by dumping stack and terminating.
+        """
+        for plugin in plugins:
+            plugin.leave_cluster()
+    signal.signal(signal.SIGQUIT, sigusr1_handler)
+
 def main(args):
     _log = logging.getLogger("metaswitch.clearwater.cluster_manager.main")
     arguments = docopt(__doc__, argv=args)
@@ -46,7 +58,7 @@ def main(args):
 
     logging_config.configure_logging(log_level, log_dir, "cluster-manager")
     utils.install_sigusr1_handler("cluster-manager")
-    
+
     # Drop a pidfile.
     pid = os.getpid()
     with open(arguments['--pidfile'], "w") as pidfile:
@@ -54,18 +66,23 @@ def main(args):
 
     plugin_holder = []
     load_plugins_in_dir("/home/vagrant/python_plugins/", plugin_holder)
+    synchronizers = []
     threads = []
     for plugin in plugin_holder:
         syncer = FakeEtcdSynchronizer(plugin, listen_ip)
         thread = Thread(target=syncer.main)
         thread.daemon = True
         thread.start()
+
+        synchronizers.append(syncer)
         threads.append(thread)
         _log.info("Loaded plugin %s" % plugin)
 
+    install_sigquit_handler(synchronizers)
+
     while True:
-      for thread in threads:
-        thread.join(1)
+        for thread in threads:
+            thread.join(1)
 
 if __name__ == '__main__':
     import sys
