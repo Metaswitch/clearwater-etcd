51b6a15eafc04125e57ab1a56ee26447d1ac4be5
Rob Day <Robert.Day@metaswitch.com>
2015-05-03 18:46:56 +0000

    Add diagnostic/recovery/upgrade scripts

---
+++ clearwater-cluster-manager.root/usr/share/clearwater/cluster_manager/scripts/mark_node_failed.py
@@ -0,0 +1,15 @@
+import sys
+from metaswitch.clearwater.cluster_manager.etcd_synchronizer import \
+    EtcdSynchronizer
+import etcd
+
+local_ip = sys.argv[1]
+key = sys.argv[2]
+dead_node_ip = sys.argv[3]
+error_syncer = EtcdSynchronizer(NullPlugin(None), '10.0.0.2')
+error_syncer.mark_node_failed()
+
+c = etcd.Client(local_ip, 4001)
+new_state = c.get(key).value
+
+print "New etcd state (after removing %s) is %s" % (dead_node_ip, new_state)
