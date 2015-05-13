1e45cf57080aae58d79cc6e7f8441945bac57ef8
Andy Caldwell <andrew.caldwell@metaswitch.com>
2015-05-13 18:27:51 +0000

    Naive port from cluster to config

---
+++ src/metaswitch/clearwater/config_manager/etcd_synchronizer.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+
+# Project Clearwater - IMS in the Cloud
+# Copyright (C) 2015 Metaswitch Networks Ltd
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version, along with the "Special Exception" for use of
+# the program along with SSL, set forth below. This program is distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details. You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# The author can be reached by email at clearwater@metaswitch.com or by
+# post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+#
+# Special Exception
+# Metaswitch Networks Ltd  grants you permission to copy, modify,
+# propagate, and distribute a work formed by combining OpenSSL with The
+# Software, or a work derivative of such a combination, even if such
+# copying, modification, propagation, or distribution would otherwise
+# violate the terms of the GPL. You must comply with the GPL in all
+# respects for all of the code used other than OpenSSL.
+# "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+# Project and licensed under the OpenSSL Licenses, or a work based on such
+# software and licensed under the OpenSSL Licenses.
+# "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+# under which the OpenSSL Project distributes the OpenSSL toolkit software,
+# as those licenses appear in the file LICENSE-OPENSSL.
+
+import etcd
+from threading import Thread
+from time import sleep
+
+from .constants import *
+import urllib3
+import logging
+
+_log = logging.getLogger("etcd_sync")
+
+
+class EtcdSynchronizer(object):
+    PAUSE_BEFORE_RETRY = 30
+
+    def __init__(self, plugin, ip, etcd_ip=None, force_leave=False):
+        self._ip = ip
+        if etcd_ip:
+            self._client = etcd.Client(etcd_ip, 4000)
+        else:
+            self._client = etcd.Client(ip, 4000)
+        self._key = plugin.key()
+        self._index = None
+        self._terminate_flag = False
+        self.thread = Thread(target=self.main)
+        self.force_leave = force_leave
+
+    def start_thread(self):
+        self.thread.daemon = True
+        self.thread.start()
+
+    def terminate(self):
+        self._terminate_flag = True
+        self.thread.join()
+
+    def main(self):
+        # Continue looping while the FSM is running.
+        while self._fsm.is_running():
+            # This blocks on changes to the watched key in etcd.
+            _log.debug("Waiting for change from etcd for key{}".format(
+                         self._key))
+            value = self.read_from_etcd()
+            if self._terminate_flag:
+                break
+            _log.debug("Got new config value from etcd:\n{}" % value)
+            self._plugin.on_config_change(value)
+
+    # Read the current value of the key from etcd (blocks until there's a
+    # change).
+    def read_from_etcd(self):
+        value = None
+        try:
+            result = self._client.get(self._key)
+
+            # If the key hasn't changed since we last saw it, then
+            # wait for it to change before doing anything else.
+            _log.info("Read config value for {} from etcd (epoch {})".format(
+                        self._key,
+                        result.modifiedIndex))
+
+            if result.modifiedIndex == self._index:
+                while not self._terminate_flag:
+                    try:
+                        result = self._client.watch(self._key,
+                                                    index=result.modifiedIndex+1,
+                                                    timeout=5,
+                                                    recursive=False)
+                        break
+                    except urllib3.exceptions.TimeoutError:
+                        # Timeouts after 5 seconds are expected, so ignore them
+                        # - unless we're terminating, we'll stay in the while
+                        # loop and try again
+                        pass
+                    except ValueError:
+                        # The index isn't valid to watch on, probably because
+                        # there has been a snapshot between the get and the
+                        # watch. Just start the read again.
+                        _log.info("etcd index {} is invalid, retrying".format(
+                            result.modifiedIndex+1))
+                        self.read_from_etcd()
+
+                # Return if we're terminating.
+                if self._terminate_flag:
+                    return
+
+            # Save off the index of the result we're using for when we write
+            # back to etcd later.
+            self._index = result.modifiedIndex
+            value = result.value
+
+        except etcd.EtcdKeyError:
+            # If the key doesn't exist in etcd then there is currently no
+            # config.
+            self._index = None
+            pass
+        except Exception as e:
+            # Catch-all error handler (for invalid requests, timeouts, etc -
+            # start over.
+            _log.error("{} caught {!r} when trying to read with index {}"
+                       " - pause before retry".
+                       format(self._ip, e, self._index))
+            # Sleep briefly to avoid hammering a failed server
+            sleep(self.PAUSE_BEFORE_RETRY)
+            # The main loop (which reads from etcd in a loop) should call this
+            # function again after we return, causing the read to be retried.
+
+        return value
---
+++ src/metaswitch/clearwater/config_manager/main.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+# Project Clearwater - IMS in the Cloud
+# Copyright (C) 2015 Metaswitch Networks Ltd
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version, along with the "Special Exception" for use of
+# the program along with SSL, set forth below. This program is distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details. You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# The author can be reached by email at clearwater@metaswitch.com or by
+# post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+#
+# Special Exception
+# Metaswitch Networks Ltd  grants you permission to copy, modify,
+# propagate, and distribute a work formed by combining OpenSSL with The
+# Software, or a work derivative of such a combination, even if such
+# copying, modification, propagation, or distribution would otherwise
+# violate the terms of the GPL. You must comply with the GPL in all
+# respects for all of the code used other than OpenSSL.
+# "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+# Project and licensed under the OpenSSL Licenses, or a work based on such
+# software and licensed under the OpenSSL Licenses.
+# "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+# under which the OpenSSL Project distributes the OpenSSL toolkit software,
+# as those licenses appear in the file LICENSE-OPENSSL.
+
+"""Clearwater Config Manager
+
+Usage:
+  main.py --local-ip=IP [--foreground] [--log-level=LVL] [--log-directory=DIR]
+          [--pidfile=FILE]
+
+Options:
+  -h --help                   Show this screen.
+  --local-ip=IP               IP address
+  --foreground                Don't daemonise
+  --log-level=LVL             Level to log at, 0-4 [default: 3]
+  --log-directory=DIR         Directory to log to [default: ./]
+  --pidfile=FILE              Pidfile to write [default: ./config-manager.pid]
+
+"""
+
+from docopt import docopt
+
+from metaswitch.common import logging_config, utils
+from metaswitch.clearwater.config_manager.plugin_loader \
+    import load_plugins_in_dir
+from metaswitch.clearwater.cluster_manager.etcd_synchronizer \
+    import EtcdSynchronizer
+import logging
+import os
+from threading import Thread
+import signal
+
+_log = logging.getLogger("metaswitch.clearwater.config_manager.main")
+
+LOG_LEVELS = {'0': logging.CRITICAL,
+              '1': logging.ERROR,
+              '2': logging.WARNING,
+              '3': logging.INFO,
+              '4': logging.DEBUG}
+
+
+def install_sigquit_handler(plugins):
+    def sigquit_handler(sig, stack):
+        _log.debug("Handling SIGQUIT")
+    signal.signal(signal.SIGQUIT, sigquit_handler)
+
+
+def main(args):
+    arguments = docopt(__doc__, argv=args)
+
+    log_dir = arguments['--log-directory']
+    log_level = LOG_LEVELS.get(arguments['--log-level'], logging.DEBUG)
+
+    stdout_err_log = os.path.join(log_dir, "config-manager.output.log")
+
+    if not arguments['--foreground']:
+        utils.daemonize(stdout_err_log)
+
+    logging_config.configure_logging(log_level, log_dir, "config-manager")
+    utils.install_sigusr1_handler("config-manager")
+
+    # Drop a pidfile.
+    pid = os.getpid()
+    with open(arguments['--pidfile'], "w") as pidfile:
+        pidfile.write(str(pid) + "\n")
+
+    plugins_dir = "/usr/share/clearwater/clearwater-config-manager/plugins/"
+    plugins = load_plugins_in_dir(plugins_dir)
+    plugins.sort(key=lambda x: x.key())
+    synchronizers = []
+    threads = []
+    for plugin in plugins:
+        syncer = EtcdSynchronizer(plugin)
+        thread = Thread(target=syncer.main)
+        thread.daemon = True
+        thread.start()
+
+        synchronizers.append(syncer)
+        threads.append(thread)
+        _log.info("Loaded plugin %s" % plugin)
+
+    install_sigquit_handler(synchronizers)
+
+    while True:
+        for thread in threads:
+            thread.join(1)
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
