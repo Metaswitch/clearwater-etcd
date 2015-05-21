cd91963606ce326ac7598b0c44f6ac22544347a9
Rob Day <Robert.Day@metaswitch.com>
2015-05-21 18:26:05 +0000

    Usability improvements to mark_node_failed script

--- clearwater-cluster-manager.root/usr/share/clearwater/clearwater-cluster-manager/scripts/mark_node_failed.py
+++ clearwater-cluster-manager.root/usr/share/clearwater/clearwater-cluster-manager/scripts/mark_node_failed.py
@@ -12,14 +12,10 @@ def make_key(site, node_type, datatore):
     else:
         return "/clearwater/{}/{}/clustering/{}".format(site, node_type, datastore)
 
-root_log = logging.getLogger()
-root_log.setLevel(logging.INFO)
-
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-root_log.addHandler(ch)
+print "Detailed output being sent to mark_node_failed.log"
+logging.basicConfig(filename="mark_node_failed.log",
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
 
 local_ip = sys.argv[1]
 site = sys.argv[2]
@@ -28,20 +24,23 @@ datastore = sys.argv[4]
 dead_node_ip = sys.argv[5]
 
 key = make_key(site, node_type, datastore)
-print "Using etcd key %s" % (key)
+logging.info("Using etcd key %s" % (key))
 
 error_syncer = EtcdSynchronizer(NullPlugin(key), dead_node_ip, etcd_ip=local_ip, force_leave=True)
 
+print "Marking node as failed to allow current operation to complete..."
 # Move the dead node into ERROR state to allow in-progress operations to
 # complete
 error_syncer.mark_node_failed()
 
 # Move the dead node out of the cluster
 error_syncer.start_thread()
+print "Beginning process of leaving the cluster - will take at least 30 seconds"
 error_syncer.leave_cluster()
 
 # Wait for it to leave
 error_syncer.thread.join()
+print "Process complete - %s has left the cluster" % dead_node_ip
 
 c = etcd.Client(local_ip, 4000)
 new_state = c.get(key).value
