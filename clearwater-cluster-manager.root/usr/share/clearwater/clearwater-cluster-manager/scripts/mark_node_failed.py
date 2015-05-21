579427e7ab992fbb71a4ba93abb964e8a7ad1b5f
Rob Day <Robert.Day@metaswitch.com>
2015-05-21 10:03:20 +0000

    Make mark_node_failed script more usable

--- clearwater-cluster-manager.root/usr/share/clearwater/clearwater-cluster-manager/scripts/mark_node_failed.py
+++ clearwater-cluster-manager.root/usr/share/clearwater/clearwater-cluster-manager/scripts/mark_node_failed.py
@@ -6,13 +6,24 @@ from metaswitch.clearwater.cluster_manager.null_plugin import \
 import etcd
 import logging
 
+def make_key(site, node_type, datatore):
+    if datastore == "cassandra":
+        return "/clearwater/{}/clustering/{}".format(node_type, datastore)
+    else:
+        return "/clearwater/{}/{}/clustering/{}".format(site, node_type, datastore)
 
 root_log = logging.getLogger()
 root_log.setLevel(logging.INFO)
 
 local_ip = sys.argv[1]
-key = sys.argv[2]
-dead_node_ip = sys.argv[3]
+site = sys.argv[2]
+node_type = sys.argv[3]
+datastore = sys.argv[4]
+dead_node_ip = sys.argv[5]
+
+key = make_key(site, node_type, datastore)
+print "Using etcd key %s" % (key)
+
 error_syncer = EtcdSynchronizer(NullPlugin(key), local_ip, etcd_ip=dead_node_ip, force_leave=True)
 error_syncer.mark_node_failed()
 
