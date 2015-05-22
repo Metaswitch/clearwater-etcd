59479b55648971abf659dcf18ab9dad23fa53401
Rob Day <Robert.Day@metaswitch.com>
2015-05-22 17:51:58 +0000

    Review markups, and mark_remote_node_failed script

--- clearwater-cluster-manager.root/usr/share/clearwater/clearwater-cluster-manager/scripts/mark_node_failed.py
+++ clearwater-cluster-manager.root/usr/share/clearwater/clearwater-cluster-manager/scripts/mark_node_failed.py
@@ -12,8 +12,9 @@ def make_key(site, node_type, datatore):
     else:
         return "/clearwater/{}/{}/clustering/{}".format(site, node_type, datastore)
 
-print "Detailed output being sent to mark_node_failed.log"
-logging.basicConfig(filename="mark_node_failed.log",
+logfile = "/var/log/clearwater-etcd/mark_node_failed.log"
+print "Detailed output being sent to %s" % logfile
+logging.basicConfig(filename=logfile,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.DEBUG)
 
@@ -28,14 +29,13 @@ logging.info("Using etcd key %s" % (key))
 
 error_syncer = EtcdSynchronizer(NullPlugin(key), dead_node_ip, etcd_ip=local_ip, force_leave=True)
 
-print "Marking node as failed to allow current operation to complete..."
+print "Marking node as failed and removing it from the cluster - will take at least 30 seconds"
 # Move the dead node into ERROR state to allow in-progress operations to
 # complete
 error_syncer.mark_node_failed()
 
 # Move the dead node out of the cluster
 error_syncer.start_thread()
-print "Beginning process of leaving the cluster - will take at least 30 seconds"
 error_syncer.leave_cluster()
 
 # Wait for it to leave
@@ -45,4 +45,4 @@ print "Process complete - %s has left the cluster" % dead_node_ip
 c = etcd.Client(local_ip, 4000)
 new_state = c.get(key).value
 
-print "New etcd state (after removing %s) is %s" % (dead_node_ip, new_state)
+logging.info("New etcd state (after removing %s) is %s" % (dead_node_ip, new_state))
