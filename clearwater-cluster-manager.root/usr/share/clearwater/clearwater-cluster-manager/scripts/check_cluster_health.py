70f46446ddf0e1c01c8ba0f7ccc7101eb820431f
Eleanor Merry <eleanor.merry@metaswitch.com>
2015-05-22 15:56:31 +0000

    Updates after review

--- clearwater-cluster-manager.root/usr/share/clearwater/clearwater-cluster-manager/scripts/check_cluster_health.py
+++ clearwater-cluster-manager.root/usr/share/clearwater/clearwater-cluster-manager/scripts/check_cluster_health.py
@@ -70,9 +70,9 @@ def describe_cluster(node_type, site, store_name):
         return
 
     if site != "":
-        print "Describing the {} {} cluster in site {}:".format(node_type.capitalize(), store_name.capitalize(), site) 
+        print "Describing the {} {} cluster in site {}:".format(node_type.capitalize(), store_name.capitalize(), site)
     else:
-        print "Describing the {} {} cluster:".format(node_type.capitalize(), store_name.capitalize()) 
+        print "Describing the {} {} cluster:".format(node_type.capitalize(), store_name.capitalize())
 
     cluster = json.loads(result.value)
     cluster_ok = all([state == "normal"
