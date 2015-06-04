b6e1c02c4d733749cc18904231fcf9ed4ad7a2d7
Rob Day <Robert.Day@metaswitch.com>
2015-06-04 15:15:55 +0000

    Add log numbers

--- src/metaswitch/clearwater/cluster_manager/pdlogs.py
+++ src/metaswitch/clearwater/cluster_manager/pdlogs.py
@@ -32,34 +32,40 @@
 
 from metaswitch.common.pdlogs import PDLog
 
-STARTUP = PDLog(desc="clearwater-cluster-manager has started",
+STARTUP = PDLog(number=PDLog.CL_CLUSTER_MGR_ID+1,
+                desc="clearwater-cluster-manager has started",
                 cause="The application is starting",
                 effect="Normal",
                 action="None",
                 priority=PDLog.LOG_NOTICE)
-EXITING = PDLog(desc="clearwater-cluster-manager is exiting",
+EXITING = PDLog(number=PDLog.CL_CLUSTER_MGR_ID+2,
+                desc="clearwater-cluster-manager is exiting",
                 cause="The application is exiting",
                 effect="Datastore cluster management services are no longer available",
                 action="This occurs normally when the application is stopped. Wait for monit to restart the application",
                 priority=PDLog.LOG_ERR)
-EXITING_BAD_CONFIG = PDLog(desc="clearwater-cluster-manager is exiting due to bad configuration",
+EXITING_BAD_CONFIG = PDLog(number=PDLog.CL_CLUSTER_MGR_ID+3,
+                           desc="clearwater-cluster-manager is exiting due to bad configuration",
                            cause="clearwater-cluster-manager was started with incorrect configuration",
                            effect="Datastore cluster management services are no longer available",
                            action="Verify that the configuration files in /etc/clearwater/ are correct according to the documentation. In particular, ensure that local_ip, management_local_ip, log_level, local_site_name and remote_site_name are set correctly.",
                            priority=PDLog.LOG_ERR)
-NODE_JOINING = PDLog(desc="A node is joining a datastore cluster",
+NODE_JOINING = PDLog(number=PDLog.CL_CLUSTER_MGR_ID+4,
+                     desc="A node is joining a datastore cluster",
                      cause="Node {ip} has started to join the {cluster_desc}",
                      effect="Normal",
                      action="None",
                      priority=PDLog.LOG_NOTICE)
-NODE_LEAVING = PDLog(desc="A node is leaving a datastore cluster",
+NODE_LEAVING = PDLog(number=PDLog.CL_CLUSTER_MGR_ID+5,
+                     desc="A node is leaving a datastore cluster",
                      cause="Node {ip} has started to leave the {cluster_desc}",
                      effect="Normal",
                      action="None",
                      priority=PDLog.LOG_NOTICE)
 
 NOT_YET_CLUSTERED_ALARM = \
-    PDLog(desc="This node is not yet clustered",
+    PDLog(number=PDLog.CL_CLUSTER_MGR_ID+6,
+          desc="This node is not yet clustered",
           cause="This node has not yet joined the {cluster_description} cluster",
           effect="This node will alarm until the cluster is joined",
           action="Wait for this node to join the cluster. If this "+\
@@ -69,7 +75,8 @@ NOT_YET_CLUSTERED_ALARM = \
           priority=PDLog.LOG_ERR)
 
 TOO_LONG_ALARM = \
-    PDLog(desc="A scaling operation has taken too long",
+    PDLog(number=PDLog.CL_CLUSTER_MGR_ID+7,
+          desc="A scaling operation has taken too long",
           cause="A scaling operation for the {cluster_description} cluster has been in progress for more than 15 minutes.",
           effect="This node will alarm until the operation finishes. No further scaling operations can begin until this one completes.",
           action="Ensure that clearwater-etcd is running, and fix any errors relating to it. If any nodes are temporarily failed, recover them. If any nodes are permanently failed, follow the documentation to remove them from the cluster.",
