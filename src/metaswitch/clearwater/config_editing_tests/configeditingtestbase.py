9fb5b10999a0bbfc7a2221668c0fa50affe5479a
Ken Ferguson <ken.ferguson@metaswitch.com>
2017-09-28 14:19:54 +0100

    Shared config unit test stub

---
+++ src/metaswitch/clearwater/config_editing_tests/configeditingtestbase.py
@@ -0,0 +1,16 @@
+# Copyright (C) Metaswitch Networks 2017
+# If license terms are provided to you in a COPYING file in the root directory
+# of the source code repository by which you are accessing this code, then
+# the license outlined in that COPYING file applies to your use.
+# Otherwise no rights are granted except for those provided to you by
+# Metaswitch Networks in a separate written agreement.
+
+import unittest
+from metaswitch.common.logging_config import configure_test_logging
+
+configure_test_logging()
+
+
+class ConfigEditingTestBase(unittest.TestCase):
+    def test_nothing(self):
+        pass
