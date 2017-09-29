20ba9d93c50bc26403e621ff3840c007fdbe48a0
Michael-Duppre <michael.duppre@metaswitch.com>
2017-09-29 17:43:20 +0100

    Small refactoring of get_user_download_dir

--- clearwater-config-manager.root/usr/share/clearwater/clearwater-config-manager/scripts/cw_config.py
+++ clearwater-config-manager.root/usr/share/clearwater/clearwater-config-manager/scripts/cw_config.py
@@ -14,7 +14,6 @@ import logging
 import sys
 
 # Constants
-SHARED_CONFIG_PATH = "/etc/clearwater/shared_config"
 DOWNLOADED_CONFIG_PATH = " ~/clearwater-config-manager/staging"
 MAXIMUM_CONFIG_SIZE = 100000
 VALIDATION_SCRIPTS_FOLDER = "/usr/share/clearwater/clearwater-config-manager/scripts/config_validation/"
@@ -28,6 +27,7 @@ shared config, re-apply the changes and try again."""
 # Set up logging
 logging.basicConfig(filename=LOG_PATH, level=logging.DEBUG)
 log = logging.getLogger(__name__)
+
 
 # Exceptions
 class ConfigAlreadyDownloaded(Exception):
@@ -66,8 +66,7 @@ class ConfigLoader(object):
         # etcd API that will get us our config.
         self._etcd_client = etcd_client
         self.prefix = "/".join(["", etcd_key, site, "configuration"])
-        self.download_dir = os.path.join(DOWNLOADED_CONFIG_PATH,
-                                         get_user_name())
+        self.download_dir = get_user_download_dir()
 
     def download_config(self, config_type):
         """Save a copy of a given config type to the download directory.
@@ -228,9 +227,8 @@ def download_config(config_loader, config_type, autoskip):
     Downloads the config from etcd and saves a copy to
     DOWNLOADED_CONFIG_PATH/<USER_NAME>.
     """
-    local_config_path = os.path.join(DOWNLOADED_CONFIG_PATH,
-                                     get_user_name(),
-                                     config_type)
+    local_config_path = os.path.join(get_user_download_dir(), config_type)
+
     if os.path.exists(local_config_path):
         # Ask user to confirm if they want to overwrite the file
         # Continue with download if user confirms
@@ -283,9 +281,7 @@ def upload_config(config_loader, config_type, force=False):
     # can be made. But need to confirm that is in fact the case.
 
     # Check that the file exists.
-    if not os.path.exists(os.path.join(DOWNLOADED_CONFIG_PATH,
-                                       get_user_name(),
-                                       config_type)):
+    if not os.path.exists(os.path.join(get_user_download_dir(), config_type)):
         raise IOError("No shared config found, unable to upload")
 
     # Log the changes.
@@ -366,6 +362,11 @@ def get_user_name():
     process = subprocess.Popen(["who", "am", "i"], stdout=subprocess.PIPE)
     output, error = process.communicate()
     return output.split()[0]
+
+
+def get_user_download_dir():
+    """Returns the user-specific directory for downloaded config."""
+    return os.path.join(DOWNLOADED_CONFIG_PATH, get_user_name())
 
 
 def print_diff(string_1, string_2):
