4d6bb10ddb6a9d1a0e85e0fcb8956d96049a4f33
Rob Day <Robert.Day@metaswitch.com>
2018-01-18 19:13:02 +0000

    Add run_commands which works in parallel

--- src/metaswitch/clearwater/etcd_shared/plugin_utils.py
+++ src/metaswitch/clearwater/etcd_shared/plugin_utils.py
@@ -15,7 +15,7 @@ import logging
 _log = logging.getLogger("etcd_shared.plugin_utils")
 
 
-def run_command(command_args, namespace=None, log_error=True):
+def run_commands(list_of_command_args, namespace=None, log_error=True):
     """Runs the given shell command, logging the output and return code.
 
     If a namespace is supplied the command is run in the specified namespace.
@@ -24,34 +24,47 @@ def run_command(command_args, namespace=None, log_error=True):
     call without shell, to avoid shell injection. Ensure the command is passed 
     in as an array instead of a string.
     """
-    if namespace:
-        command_args[0:0] = ['ip', 'netns', 'exec', namespace]
+    processes = []
+    error_returncodes = []
+    for command_args in list_of_command_args:
+        if namespace:
+            command_args[0:0] = ['ip', 'netns', 'exec', namespace]
 
-    # Pass the close_fds argument to avoid the pidfile lock being held by
-    # child processes
-    p = subprocess.Popen(command_args,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE,
-                         close_fds=True)
-    stdout, stderr = p.communicate()
-    if p.returncode != 0:
-        # it failed, log the return code and output
-        if log_error:
-            _log.error("Command {} failed with return code {}, "
-                       "stdout {!r}, and stderr {!r}".format(' '.join(command_args),
-                                                             p.returncode,
-                                                             stdout,
-                                                             stderr))
-            return p.returncode
-    else:
-        # it succeeded, log out stderr of the command run if present
-        if stderr:
-            _log.warning("Command {} succeeded, with stderr output {!r}".
-                         format(' '.join(command_args), stderr))
+        # Pass the close_fds argument to avoid the pidfile lock being held by
+        # child processes
+        p = subprocess.Popen(command_args,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             close_fds=True)
+        processes.append(p)
+
+    for p in processes:
+        stdout, stderr = p.communicate()
+        if p.returncode != 0:
+            # it failed, log the return code and output
+            if log_error:
+                _log.error("Command {} failed with return code {}, "
+                           "stdout {!r}, and stderr {!r}".format(' '.join(command_args),
+                                                                 p.returncode,
+                                                                 stdout,
+                                                                 stderr))
+            error_returncodes.append(p.returncode)
         else:
-            _log.debug("Command {} succeeded".format(' '.join(command_args)))
+            # it succeeded, log out stderr of the command run if present
+            if stderr:
+                _log.warning("Command {} succeeded, with stderr output {!r}".
+                             format(' '.join(command_args), stderr))
+            else:
+                _log.debug("Command {} succeeded".format(' '.join(command_args)))
 
+    if error_returncodes:
+        return error_returncodes[0]
+    else:
         return 0
+
+
+def run_command(command_args, namespace=None, log_error=True):
+    return run_commands([command_args], namespace=namespace, log_error=log_error)
 
 
 def safely_write(filename, contents, permissions=0644):
